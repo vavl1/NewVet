f28b17f42ce4c20d0f99bce3dfa2dea3284a64bf
vavl <vlasolu150@gmail.com>
2023-02-20 21:41:49 +0300

    ав

--- Vet/Areas/Admin/Controllers/WorkerController.cs
+++ Vet/Areas/Admin/Controllers/WorkerController.cs
@@ -18,6 +18,18 @@ namespace Vet.Areas.Admin.Controllers
            
             return View(inspections);
         }
+        public async Task<IActionResult> CanselTreatment(int? animalid, int? treatmentid, int? vetId)
+        {
+            var pet = await new AnimalDal().GetAsync(animalid.GetValueOrDefault());
+            pet.VetId = null;
+            await new AnimalDal().AddOrUpdateAsync(pet);
+            var treatment = await new TreatmetsDal().GetAsync(treatmentid.GetValueOrDefault());
+            treatment.DateEnd = DateTime.Now;
+            await new TreatmetsDal().AddOrUpdateAsync(treatment);
+
+           
+            return Redirect("/Admin/Worker/Index/" +vetId);
+        }
         [HttpPost]
         public async Task<IActionResult> CreateTreatment(TreatmentModel model)
         {
@@ -30,7 +42,7 @@ namespace Vet.Areas.Admin.Controllers
                     AnimalId = model.AnimalId,
                     VetId = model.VetId,
                     Date = DateTime.Now,
-
+                    Name = model.Diagnos,
                 });
                 var treatment = await new TreatmetsDal().AddOrUpdateAsync(new Entities.TreatmentEntity
                 {
