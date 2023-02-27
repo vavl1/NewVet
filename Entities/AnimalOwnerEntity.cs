46e10f89ae3b80346d9e0eeff0b73f2a04004cf8
vavl <vlasolu150@gmail.com>
2023-02-27 17:18:08 +0300

    чет с валидацией понаделал

--- Entities/AnimalOwnerEntity.cs
+++ Entities/AnimalOwnerEntity.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Entities
 {
     public class AnimalOwnerEntity
     {
+        [Display(Name = "Ид")]
+        
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+       
         public string Name { get; set; } = null!;
-
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+      
         public string? LastName { get; set; }
-
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        
         public string? FatherName { get; set; }
-
+        [Display(Name = "Телефон")]
         public string? Phone { get; set; }
-
+        [Display(Name = "Адресс")]
         public string? Adress { get; set; }
-
+    
         public virtual ICollection<AnimalEntity> Animals { get; set; } = new List<AnimalEntity>();
     }
 }
--- Entities/VetEntity.cs
+++ Entities/VetEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,33 @@ namespace Entities
 {
     public class VetEntity
     {
+        [Display(Name="Ид")]
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+    
         public string Name { get; set; } = null!;
-
+        [Required(ErrorMessage = "Введите Фамилию")]
+        [Display(Name = "Фамилия")]
+      
         public string? LastName { get; set; }
-
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+       
         public string? FatherName { get; set; }
-
+        [Display(Name = "Телефон")]
         public string? Phone { get; set; }
-
+        [Display(Name = "Роль")]
         public RoleType? RoleType { get; set; }
+        [Required(ErrorMessage = "Введите Логин")]
+        [Display(Name = "Логин")]
+      
         public string? Login { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
+        [Display(Name = "Пароль")]
+        
         public string? Password { get; set; }
+        [Display(Name = "Изображение")]
         public string? PhotoParth { get; set; }
 
         public virtual ICollection<AnimalEntity> Animals { get; } = new List<AnimalEntity>();
